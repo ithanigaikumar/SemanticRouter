289f5bbdd949edfed906598ad67c9c8acf846021
ithanigaikumar <107815119+ithanigaikumar@users.noreply.github.com>
2024-04-19 16:35:32 +0100

    routing

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from unify import ChatBot
 def main():
     st.sidebar.title("Configuration")
     # Load API key from environment or prompt for it if not available
-    unify_key = os.getenv("UNIFY_KEY") or st.sidebar.text_input(
+    unify_key = st.sidebar.text_input(
         "Enter your UNIFY_KEY", value="")
 
     if unify_key:
@@ -14,7 +14,7 @@ def main():
             # Initialize the ChatBot with the specified endpoint and API key
             chatbot = ChatBot(
                 api_key=unify_key,
-                endpoint="gpt-3.5-turbo@lowest-input-cost"
+                endpoint="llama-2-13b-chat@anyscale"
             )
             st.title("Streaming Router ChatBot")
 
@@ -30,16 +30,18 @@ def main():
             # User input for the message
             user_input = st.text_input(
                 "Type your message here:", key="user_input")
-
+            print(f"This is {user_input}")
             # Handling sending message
             if st.button("Send") and user_input:
                 st.session_state.history.append(f"You: {user_input}")
-
+                print(
+                    f"Session state before response{st.session_state.history}")
                 # Get the bot's response
                 # Ensure .run() is an existing method
                 response = chatbot.run(user_input)
+                print(f"This is {response}")
                 st.session_state.history.append(f"Bot: {response}")
-
+                print(f"After response{st.session_state.history}")
                 # Refresh to show updated chat history
                 st.experimental_rerun()
         except Exception as e:
