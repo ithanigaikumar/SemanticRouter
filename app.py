796556fbe3eddd2cdace3ca9f983e4c92171253f
ithanigaikumar <107815119+ithanigaikumar@users.noreply.github.com>
2024-04-20 19:17:29 +0100

    Full streamlit app with chatbot functionality integrated with routing

--- app.py
+++ app.py
@@ -1,25 +1,109 @@
 import streamlit as st
 import asyncio
 from unify import AsyncUnify
+import os
+from semantic_router import Route
+from getpass import getpass
+from semantic_router import RouteLayer
+from semantic_router.encoders import OpenAIEncoder
+
 
 # Asynchronously handle chat operations
 
 
-async def async_chat(api_key, user_input):
-    async_unify = AsyncUnify(
-        api_key=api_key, endpoint="llama-2-13b-chat@anyscale")
-    # Ensure this is async and properly awaited so doesn't block application
-    response = await async_unify.generate(user_prompt=user_input)
+async def async_chat(openai_api_key, api_key, user_input, routes):
+    # encoder = CohereEncoder()
+    os.environ["OPENAI_API_KEY"] = openai_api_key
+    encoder = OpenAIEncoder()
 
-    # If response is a string and not a stream, handle it directly
-    if isinstance(response, str):
-        return response
+    rl = RouteLayer(encoder=encoder, routes=routes)
+    route_choice = rl(user_input)
+    print(f"Route chosen: {route_choice}")
 
-    # If response is a stream, then iterate over it
-    response_text = ''
-    async for chunk in response:
-        response_text += chunk
-    return response_text
+    if route_choice.name == "math":
+        # Initialize Unify with the endpoint for math queries
+        unify = AsyncUnify(
+            api_key=api_key,
+            # Use the correct endpoint for math queries
+            endpoint="llama-2-13b-chat@anyscale"
+        )
+        # Generate the response using Unify
+        response = await unify.generate(user_prompt=user_input)
+        # If response is a string and not a stream, handle it directly
+        if isinstance(response, str):
+            return response
+
+        # If response is a stream, then iterate over it
+        response_text = ''
+        async for chunk in response:
+            response_text += chunk
+        return response_text
+
+    elif route_choice.name == "coding":
+        # Initialize Unify with the endpoint for coding queries
+        unify = AsyncUnify(
+            api_key=api_key,
+            # Use the correct endpoint for coding queries
+            endpoint="codellama-34b-instruct@anyscale"
+        )
+        # Generate the response using Unify
+        response = await unify.generate(user_prompt=user_input)
+        # If response is a string and not a stream, handle it directly
+        if isinstance(response, str):
+            return response
+
+        # If response is a stream, then iterate over it
+        response_text = ''
+        async for chunk in response:
+            response_text += chunk
+        return response_text
+
+    else:
+        unify = AsyncUnify(
+            api_key=api_key,
+            # Use the correct endpoint for coding queries
+            endpoint="llama-2-13b-chat@anyscale"
+        )
+        # Generate the response using Unify
+        response = await unify.generate(user_prompt=user_input)
+        # If response is a string and not a stream, handle it directly
+        if isinstance(response, str):
+            return response
+
+        # If response is a stream, then iterate over it
+        response_text = ''
+        async for chunk in response:
+            response_text += chunk
+        return response_text
+
+
+def defineRoutes():
+    math_route = Route(
+        name="math",
+        utterances=[
+            "solve for x in the equation",
+            "what is the integral of",
+            "how to calculate the derivative",
+            "mathematical proofs",
+            "how do you find the percentage of this number",
+            "how do you solve the determinant of a 2x2 matrix?"
+        ],
+    )
+
+    coding_route = Route(
+        name="coding",
+        utterances=[
+            "how to code a for loop in Python",
+            "explain the use of classes in Java",
+            "what is recursion in programming",
+            "how do i optimise this problem using hash tables",
+            "suggest a more efficient data structure for this problem"
+        ],
+    )
+
+    # List of all routes
+    routes = [math_route, coding_route]
+    return routes
 
 
 def main():
@@ -46,15 +130,16 @@ def main():
 
         # Input for user message
         user_input = st.text_input("Type your message here:", key="user_input")
-
+        route_list = defineRoutes()
         # Button to send the message
         if st.button("Send") and user_input:
             st.session_state.history.append(f"You: {user_input}")
             # Process chat asynchronously
-            response = asyncio.run(async_chat(unify_key, user_input))
+            response = asyncio.run(async_chat(openai_api_key,
+                                              unify_key, user_input, route_list))
             st.session_state.history.append(f"Bot: {response}")
             # Trigger a rerun of the app to update the conversation display
-            st.experimental_rerun()
+            st.rerun()
     else:
         st.error("Please enter valid keys to start chatting.")
 
