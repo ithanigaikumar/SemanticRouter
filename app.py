ea3cded25a14ab960c6557d0c9b8c2bbff8155da
ithanigaikumar <107815119+ithanigaikumar@users.noreply.github.com>
2024-04-23 18:33:29 +0100

    Added semantic route function

--- app.py
+++ app.py
@@ -8,10 +8,30 @@ from semantic_router import RouteLayer
 from semantic_router.encoders import OpenAIEncoder
 
 
+# Routes to the appropriate endpoint
+async def semantic_route(api_key, route_endpoint, user_input):
+    unify = AsyncUnify(
+        api_key=api_key,
+        # Use the correct endpoint for math queries
+        endpoint=route_endpoint
+    )
+   # Generate the response using Unify
+    response = await unify.generate(user_prompt=user_input)
+   # If response is a string and not a stream, handle it directly
+    if isinstance(response, str):
+        return response
+
+   # If response is a stream, then iterate over it
+    response_text = ''
+    async for chunk in response:
+        response_text += chunk
+    return response_text
+
+
 # Asynchronously handle chat operations
 
 async def async_chat(openai_api_key, api_key, user_input, routes):
-    # encoder = CohereEncoder()
+   # encoder = CohereEncoder()
     os.environ["OPENAI_API_KEY"] = openai_api_key
     encoder = OpenAIEncoder()
 
@@ -20,62 +40,24 @@ async def async_chat(openai_api_key, api_key, user_input, routes):
     print(f"Route chosen: {route_choice}")
 
     if route_choice.name == "math":
-        # Initialize Unify with the endpoint for math queries
-        unify = AsyncUnify(
-            api_key=api_key,
-            # Use the correct endpoint for math queries
-            endpoint="llama-2-13b-chat@anyscale"
-        )
-        # Generate the response using Unify
-        response = await unify.generate(user_prompt=user_input)
-        # If response is a string and not a stream, handle it directly
-        if isinstance(response, str):
-            return response
 
-        # If response is a stream, then iterate over it
-        response_text = ''
-        async for chunk in response:
-            response_text += chunk
-        return response_text
+        endpoint = "llama-2-13b-chat@anyscale"
+        response = await semantic_route(api_key, endpoint, user_input)
+        return response
 
     elif route_choice.name == "coding":
-        # Initialize Unify with the endpoint for coding queries
-        unify = AsyncUnify(
-            api_key=api_key,
-            # Use the correct endpoint for coding queries
-            endpoint="codellama-34b-instruct@anyscale"
-        )
-        # Generate the response using Unify
-        response = await unify.generate(user_prompt=user_input)
-        # If response is a string and not a stream, handle it directly
-        if isinstance(response, str):
-            return response
-
-        # If response is a stream, then iterate over it
-        response_text = ''
-        async for chunk in response:
-            response_text += chunk
-        return response_text
+        # Use the correct endpoint for coding queries
+        endpoint = "codellama-34b-instruct@anyscale"
+        response = await semantic_route(api_key, endpoint, user_input)
+        return response
 
     else:
-        unify = AsyncUnify(
-            api_key=api_key,
-            # Use the correct endpoint for coding queries
-            endpoint="llama-2-13b-chat@anyscale"
-        )
-        # Generate the response using Unify
-        response = await unify.generate(user_prompt=user_input)
-        # If response is a string and not a stream, handle it directly
-        if isinstance(response, str):
-            return response
-
-        # If response is a stream, then iterate over it
-        response_text = ''
-        async for chunk in response:
-            response_text += chunk
-        return response_text
+        endpoint = "llama-2-13b-chat@anyscale"
+        response = await semantic_route(api_key, endpoint, user_input)
+        return response
 
 
+# Define routes function
 def defineRoutes():
     math_route = Route(
         name="math",
@@ -104,20 +86,22 @@ def defineRoutes():
     routes = [math_route, coding_route]
     return routes
 
+# handles send
+
 
 def handle_send():
     user_input = st.session_state.user_input
-    route_list = defineRoutes()
     if user_input:
         st.session_state.history.append(f"You: {user_input}")
+        # Asynchronously handle the chat response
         response = asyncio.run(async_chat(st.session_state.openai_api_key,
-                                          st.session_state.unify_key, user_input, route_list))
+                                          st.session_state.unify_key, user_input, defineRoutes()))
         st.session_state.history.append(f"Bot: {response}")
-    # Clear the input field by resetting the state variable
+    # Reset the input field after handling the send
     st.session_state.user_input = ""
-    st.rerun()
 
 
+# main function
 def main():
     st.sidebar.title("Configuration")
     unify_key = st.sidebar.text_input("Enter your UNIFY_KEY", type='password')
@@ -136,16 +120,14 @@ def main():
 
         for message in st.session_state.history:
             sender, text = message.split(": ", 1)
-            st.text(f"{sender}: {text}")
+            st.markdown(f"{sender}: {text}")
 
-        # The user_input is now tied to the text input's state directly
-        st.session_state.user_input = st.text_input(
-            "Type your message here:", value=st.session_state.get('user_input', ''))
+        user_input = st.text_input(
+            "Type your message here:", key="input", value="")
+        st.session_state.user_input = user_input
 
         if st.button("Send", on_click=handle_send):
-            # Button press is handled in the handle_send function
-            pass
-
+            pass  # Button press is handled in the handle_send function
     else:
         st.error("Please enter valid keys to start chatting.")
 
