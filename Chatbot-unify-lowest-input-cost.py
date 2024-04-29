06c855ad1b27575a3ed92150534f4a19bd490430
ithanigaikumar <107815119+ithanigaikumar@users.noreply.github.com>
2024-04-29 18:44:22 +0100

    cleaned up

--- Chatbot-unify-lowest-input-cost.py
+++
@@ -1,9 +0,0 @@
-from unify import ChatBot
-import os
-agent = ChatBot(
-    # This is the default and optional to include.
-    api_key="X99eQzqYps-83ouRX-9ELhM+BhDAY4tpXIkTFFXibs4=",
-    endpoint="llama-2-13b-chat@anyscale"
-)
-agent.run()
-print(agent._message_history)
