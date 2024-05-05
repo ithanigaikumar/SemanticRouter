01923f601e719044b2588113451e0ff788393be2
ithanigaikumar <107815119+ithanigaikumar@users.noreply.github.com>
2024-05-05 13:03:02 +0100

    integrated ui with dataset

--- app.py
+++ app.py
@@ -8,9 +8,20 @@ from getpass import getpass
 from semantic_router import RouteLayer
 from concurrent.futures import ThreadPoolExecutor
 from semantic_router.encoders.huggingface import HuggingFaceEncoder
+import logging
+import time
+# Configure logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-# huggingface_url = "https://api-inference.huggingface.co/models/gpt2"
-# Routes to the appropriate endpoint
+# Example usage of logging in your script
+logging.debug("This is a debug message")
+logging.info("This is an informational message")
+logging.warning("This is a warning message")
+logging.error("This is an error message")
+logging.critical("This is a critical message")
+
+
 huggingface_logo = "https://cdn-lfs.huggingface.co/repos/96/a2/96a2c8468c1546e660ac2609e49404b8588fcf5a748761fa72c154b2836b4c83/9cf16f4f32604eaf76dabbdf47701eea5a768ebcc7296acc1d1758181f71db73?response-content-disposition=inline%3B+filename*%3DUTF-8%27%27hf-logo.png%3B+filename%3D%22hf-logo.png%22%3B&response-content-type=image%2Fpng&Expires=1714669014&Policy=eyJTdGF0ZW1lbnQiOlt7IkNvbmRpdGlvbiI6eyJEYXRlTGVzc1RoYW4iOnsiQVdTOkVwb2NoVGltZSI6MTcxNDY2OTAxNH19LCJSZXNvdXJjZSI6Imh0dHBzOi8vY2RuLWxmcy5odWdnaW5nZmFjZS5jby9yZXBvcy85Ni9hMi85NmEyYzg0NjhjMTU0NmU2NjBhYzI2MDllNDk0MDRiODU4OGZjZjVhNzQ4NzYxZmE3MmMxNTRiMjgzNmI0YzgzLzljZjE2ZjRmMzI2MDRlYWY3NmRhYmJkZjQ3NzAxZWVhNWE3NjhlYmNjNzI5NmFjYzFkMTc1ODE4MWY3MWRiNzM%7EcmVzcG9uc2UtY29udGVudC1kaXNwb3NpdGlvbj0qJnJlc3BvbnNlLWNvbnRlbnQtdHlwZT0qIn1dfQ__&Signature=XlyzK%7EHZi9Vmf-w3gi8X7aNEFuV4m7qxNKtiKphVMpryDKpaZ708r1xZgMVn9tb56INExpW7gWQp9OWT1rsrcdhgB0T6WQiZvGQT4K9nl4eF8nglTJcQigmu8YOPDZqnBPOp%7E5IihQgm5-QYJfdxaMZT3JqDBsDRNiBhjj6GUHn7ye8QJu21dVsEqXL5ZU3qQUvh8Gdy%7EnPjip%7ET04mIzC0IEwPm3q7ZyA2BkeD-%7EL4LkWZ5wpsvejZQkoUU77Zklm1DcocZ8AZbRsejPshqbm%7E%7EGjhxmXHcz9Nu-AjBXDk3fnp11RDBRJlFwaTjOE9aPi8kXzL498vwUmcFzWynjg__&Key-Pair-Id=KVTP0A1DKRTAX"
 unify_logo = "https://raw.githubusercontent.com/unifyai/unifyai.github.io/main/img/externally_linked/logo.png?raw=true#gh-light-mode-only"
 
@@ -29,6 +40,8 @@ res_code = load_from_json("questionscode.json")
 
 
 async def semantic_route(api_key, route_endpoint, user_input):
+    logging.debug(
+        f"Starting semantic_route for input: {user_input} with endpoint: {route_endpoint}")
     unify = AsyncUnify(
         api_key=api_key,
         endpoint=route_endpoint
@@ -37,12 +50,16 @@ async def semantic_route(api_key, route_endpoint, user_input):
     response = await unify.generate(user_prompt=user_input)
     # If response is a string and not a stream, handle it directly
     if isinstance(response, str):
+        logging.debug("Received a direct response from the API")
         return response
 
     # If response is a stream, then iterate over it
+    logging.debug("Processing response stream")
     response_text = ''
     async for chunk in response:
         response_text += chunk
+    logging.debug(
+        f"Completed assembly of response text for input: {user_input}")
     return response_text
 
 # Re-implemented async_chat to include custom endpoints and response information with styling.
@@ -52,13 +69,17 @@ async def async_chat(huggingface_apikey, api_key, user_input, routes, endpoint="
     # Set API key environment variable at the beginning of the function, if not set globally
     os.environ["huggingface_apikey"] = huggingface_apikey
     encoder = HuggingFaceEncoder()
-    # print(f"routes in async_chat:{routes}")
-    # print(f"endpoint chosen:{endpoint}")
+    print(f"routes in async_chat:{routes}")
+    print(f"endpoint chosen:{endpoint}")
+    logging.debug(f"Starting routing for input: {user_input}")
+    start_time = time.time()  # Import time module if not already imported
     # Assuming OpenAIEncoder and RouteLayer are defined and imported properly elsewhere
     rl = RouteLayer(encoder=encoder, routes=routes)
     route_choice = rl(user_input)
-    # print(f"Route chosen: {route_choice.name}")
-
+    print(f"Route chosen: {route_choice.name}")
+    elapsed_time = time.time() - start_time
+    logging.debug(
+        f"Routing completed in {elapsed_time:.2f} seconds. Route chosen: {route_choice.name}")
     # Define specific endpoints for known route names
     endpoint_map = {
         "math": "llama-2-13b-chat",
@@ -71,7 +92,7 @@ async def async_chat(huggingface_apikey, api_key, user_input, routes, endpoint="
     else:
         # Strip any "@anyscale" from the endpoint
         chosen_endpoint = endpoint.rstrip("@anyscale")
-
+    logging.debug(f"Endpoint for processing: {chosen_endpoint}")
     # Call the semantic route function with the chosen endpoint
     response = await semantic_route(api_key, f"{chosen_endpoint}@anyscale", user_input)
 
@@ -116,7 +137,7 @@ def defineRoutes():
 
     coding_route = Route(
         name="coding",
-        utterances=res_code,
+        utterances=res_code
     )
 
     # List of all routes
@@ -164,6 +185,7 @@ def main():
         <img src='{huggingface_logo}' style='height: 40px; margin-right: 10px;' alt='HuggingFace Logo'/>
         Configuration
         <img src='{unify_logo}' style='height: 40px; margin-right: 10px;' alt='Unify Logo'/>
+        
     </div>
     """
     # Using markdown to display what acts as a sidebar title with logos
@@ -209,8 +231,8 @@ def main():
                 "Enter the name of your custom route:")
             custom_utterances = st.sidebar.text_input(
                 "Enter some examples to direct to this route (separate by comma):")
+        st.title("🤖💬 Streaming Router ChatBot")
 
-        st.title("🤖💬 Semantic Router ChatBot")
         # Initialize or update the chat history in session state
         if 'chat_history' not in st.session_state:
             st.session_state.chat_history = []
@@ -219,20 +241,12 @@ def main():
         messages_container = st.container()
         for msg_type, msg_content in st.session_state.chat_history:
             if msg_type == "user":
-                # Assuming user messages don't contain HTML
-                messages_container.write(f"User: {msg_content}")
+                messages_container.chat_message("user").write(msg_content)
             elif msg_type == "assistant":
-                # Render assistant messages as HTML
-                messages_container.markdown(
-                    msg_content, unsafe_allow_html=True)
+                messages_container.chat_message("assistant").write(msg_content)
 
         # Chat input at the bottom of the page
         user_input = st.chat_input("Say something", key="chat_input")
-
-        # Reset button to clear chat history
-        if st.button("Reset Chat"):
-            st.session_state.chat_history = []
-            st.experimental_rerun()
 
         if user_input:
             routes = defineRoutes()  # Load or define the routes applicable to this session
@@ -244,11 +258,9 @@ def main():
                 future = executor.submit(
                     async_chat_wrapper, user_input, st.session_state.huggingface_apikey, st.session_state.unify_key, routes, selected_model)
                 response = future.result()
-                # Use st.markdown to render the HTML content
-                st.markdown(response, unsafe_allow_html=True)
                 st.session_state.chat_history.append(("user", user_input))
                 st.session_state.chat_history.append(("assistant", response))
-                st.experimental_rerun()
+                st.rerun()
         else:
             st.warning("Type something to start chatting.")
     else:
